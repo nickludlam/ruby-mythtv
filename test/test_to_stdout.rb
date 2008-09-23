f0830b59518b13caeeace4a7ef71950078d7ba73
Nick Ludlam <nick@recoil.org>
2008-09-23 23:49:10 +0100

    Removed old/incomplete tests. Added a logging system to replace puts statements. Programs can now reference the owning Channel. Slightly rejigged some test statements

--- test/test_to_stdout.rb
+++
@@ -1,9 +0,0 @@
-require '../lib/ruby-mythtv'
-
-@backend = MythTV::Backend.new(:host => 'pico')
-
-all_recordings = @backend.query_recordings
-
-@backend.stream(all_recordings[-1]) do |chunk|
-  puts chunk
-end
